58d81ba6f0d182930f7fd5cf9c3fbd279fb6ef57
Donner886 <58540147+Donner886@users.noreply.github.com>
2025-12-17 22:36:00 +0800

    group dnn py311 docker evn into one folder

---
+++ dockerenv/tf23studyenv/local-starter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Startup docker containers for local development
+
+echo "Starting docker container..."
+
+## try to check if the container is already running
+CONTAINER_NAME="tf23studyenv_local"
+if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
+    echo "Container ${CONTAINER_NAME} is already running."
+    exit 0
+fi  
+## if not running, start a new container
+docker run --rm \
+    -p 8888:8888 \
+    -v /Users/madongdong/Deep-learning:/workspace \
+    --name tf23studyenv_local tf23:v0.0.2 
